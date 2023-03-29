a59673ae6110d779cb69e1c18f06513eabfcf74d
Hieu Nguyen <nhocproaoe@gmail.com>
2023-03-29 17:37:40 +0700

    add swagger ui lib

--- src/main/java/com/carrental/controller/CarController.java
+++ src/main/java/com/carrental/controller/CarController.java
@@ -1,9 +1,12 @@
 package com.carrental.controller;
 
+import com.carrental.entity.CarEntity;
 import com.carrental.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +17,10 @@ public class CarController {
     @GetMapping("/get")
     public ResponseEntity<?> get() {
         return ResponseEntity.ok().body(repository.findAll());
+    }
+
+    @PostMapping("/post")
+    public ResponseEntity<?> post(@RequestBody CarEntity car) {
+        return ResponseEntity.ok().build();
     }
 }
