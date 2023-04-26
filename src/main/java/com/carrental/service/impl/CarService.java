deb5d0ea0bdb3f08b026d2579f198a73bd1a0f5f
Hieu Nguyen <nhocproaoe@gmail.com>
2023-04-26 10:06:19 +0700

    fix: fix add new car service doesn't save car

--- src/main/java/com/carrental/service/impl/CarService.java
+++ src/main/java/com/carrental/service/impl/CarService.java
@@ -107,12 +107,11 @@ public class CarService implements ICarService {
                         .build()
         ).collect(Collectors.toList()));
         List<FeatureEntity> features = featureService.findAllByIdIn(request.getFeatureList());
-//        for (int i = 0; i < features.size(); i++) {
-//            FeatureEntity feat = features.get(i);
-//            feat.setCars(carEntity);
-//            features.set(i, feat);
-//        }
-        features.forEach(i -> i.setCars(carEntity));
+        for (int i = 0; i < features.size(); i++) {
+            FeatureEntity feat = features.get(i);
+            feat.setCars(carEntity);
+            features.set(i, feat);
+        }
         carEntity.setFeatures(features);
         CarEntity savedCar = repository.save(carEntity);
         return CarRegisterResponse.builder()
